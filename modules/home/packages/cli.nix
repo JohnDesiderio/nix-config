24ba1e94f5d7bae87ce80ae341d5c7179bf59917
JohnDesiderio <johnfrancisdesiderio@gmail.com>
2025-11-26 15:58:39 -0500

    Added more to package

--- modules/home/packages/cli.nix
+++ modules/home/packages/cli.nix
@@ -1,10 +1,20 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    # Better core utils
+    duf
+    eza
+    fd
+    gping
+    man-pages
+
     # Tools / useful cli
     tdf
 
     # Monitoring
     htop
+
+    # Utilities
+    file
   ];
 }
